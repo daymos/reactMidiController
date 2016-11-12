7dc797d5967fa3adf039082528dc33124615c51c
mattiaS <mattia@3doers.it>
2016-11-12 13:13:42 +0000

    small refactor

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import _ from 'ramda'
 import './app.css';
 
 function grid(x,y){
-  'use strict'
   return new Array(x).fill(0).map(()=> new Array(y).fill({visible:0})) 
 }
 
 class App extends Component{
+
+  update(el){
+  return (!el.visible)?{visible:1}:{visible:0}
+  }
 
   constructor(props){
     super(props)
@@ -17,7 +20,6 @@ class App extends Component{
   }
   render(){
     const {cells} =  this.state
-    console.log(this.state)
     return (
     <div>{
      cells.map((row, i)=>{ 
@@ -25,8 +27,8 @@ class App extends Component{
          let pos = this.state.cells[i][j]['visible']
          return <div className={`cell ${(pos)?'active':''}`} key={`cell${j}`} onClick={ (e)=>{
           let gridLens = _.lensPath(['cells']), rowLens = _.lensIndex(i), colLens = _.lensIndex(j);
-        this.setState(_.over(_.compose(gridLens, rowLens, colLens), (el)=>{return (!el.visible)?{visible:1}:{visible:0} },  this.state))
-         }} >{j}</div> })} </div> }) 
+        this.setState(_.over(_.compose(gridLens, rowLens, colLens), this.update ,  this.state))
+         }} >{''}</div> })} </div> }) 
     }</div>
     )
   }
