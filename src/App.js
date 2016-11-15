b202123909efe2f9dde6432afc2962af3d96c1c2
mattiaS <mattia@3doers.it>
2016-11-15 17:40:23 +0000

    add sequencer function

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
+
 import React, { Component } from 'react';
 import type { matrix, cell } from './types';
 import _ from 'ramda';
 import './app.css';
 
+//const _audioServer = 'https://soundz-server.herokuapp.com/'
+const _audioServer = 'http://localhost:3002'
 
-function initCell(a):cell{
-  return {blinking:false, playing:false, loaded:true } //cell should contain all info regarding a sound sample and its execution
+function initCell(i,j):cell{
+  return {row: i, col:j, blinking:false, playing:false, loaded:true, sample:'128_C_MelodyWoody_SP_01.wav' } //cell should contain all info regarding a sound sample and its execution
 }
 function grid(x,y):matrix{
-  return new Array(x).fill(0).map(()=> new Array(y).fill(initCell(0)))
+  return new Array(x).fill(0).map((_, i)=> new Array(y).fill(0).map((_, j)=> initCell(i, j)))
 }
 
 class App extends Component{
 
   static defaultProps = {
-    cells:grid(2,2)
+    cells:grid(2,2),
+    playing:[]
   }
 
   reducer(el){
     if(el.loaded === false){
       return el;
     }
-    else if(el.loaded === true && el.blinking === false){
-      return _.evolve({blinking:()=> true}, el)
+    if(el.loaded === true && el.blinking === false){
+      return _.evolve({blinking:()=> true, playing:()=> true}, el)
     }
-    else if (el.playing){
+    if (el.playing){
     return _.evolve({playing:()=>false, blinking:()=>true}, el)
   } else {
     console.log('defaut')
@@ -33,31 +37,38 @@ class App extends Component{
 
 }
 
+looper(){
+  //play all item into play array every given ammount of time passes
+}
+
 clickBox(i, j){
   let gridLens = _.lensPath(['cells']), rowLens = _.lensIndex(i), colLens = _.lensIndex(j);
-  this.setState(_.over(_.compose(gridLens, rowLens, colLens), this.reducer ,  this.state)) //this compose lenses to get and set new value using update
-
+  this.setState(_.over(_.compose(gridLens, rowLens, colLens), this.reducer ,  this.state), ()=>{
+  //get all value with playing == true into playing array
+   this.setState({playing: this.state.playing.concat(_.flatten(this.state.cells).filter((el)=>el.playing === true))}, ()=>console.log(this.state))
+  })
 }
+
 
 constructor(props){
   super(props)
-  const {cells} = props
-  this.state = { cells }
-
+  console.log(props)
+  this.state = { ...props }
 }
+
 render(){
   const {cells} =  this.state
-  console.log(this.state)
   return (
     <div>{
       cells.map((row, i)=>{
         return <div className='row' key={`row${i}`} > {row.map((col,j)=>{
-          let pos = this.state.cells[i][j]['blinking']
-          return <div className={`cell ${(pos)?'blinking':''}`} key={`cell${j}`} onClick={this.clickBox.bind(this, i, j)} >
-          <audio id={`r${i}c${j}`} >
-          <source src='' type="audio/ogg" ></source>
-          </audio>
-          </div> })} </div> })
+          let blinking = cells[i][j]['blinking']
+          let playing = cells[i][j]['playing']
+          return <div className={`cell ${(blinking)?'blinking':''}`} key={`cell${j}`} onClick={this.clickBox.bind(this, i, j)}  >
+            <audio id={`r${i}c${j}`} loop>
+              <source src={`${_audioServer}/${cells[i][j]['sample']}`} type="audio/ogg" ></source>
+            </audio>
+        </div> })} </div> })
         }</div>
       )
     }
